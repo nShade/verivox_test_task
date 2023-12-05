ffc75d3e57fa8954168a27aa2c118750558ded3e
Ilya Karpeev <karpeev@fastmail.com>
2023-12-05 19:04:07 +0100

    Move loading config to a plugin Modify test parameters for a better readability

---
+++ conftest.py
@@ -0,0 +1,66 @@
+import functools
+
+import yaml
+from _pytest.config import filename_arg, UsageError
+from pytest import fixture, Config, StashKey
+from yaml.parser import ParserError
+
+config_file_key = StashKey["ConfigFile"]()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--config",
+        "--cfg",
+        help="Config file to use, defaults to %(default)s",
+        action='store',
+        dest='config_file_path',
+        type=functools.partial(filename_arg, optname="--config"),
+    )
+
+
+class ConfigFilePlugin:
+    def __init__(self, path):
+        try:
+            with open(path, 'r', encoding='utf-8') as config_file:
+                self.config = yaml.safe_load(config_file)
+        except (FileNotFoundError, ParserError) as err:
+            raise RuntimeError('Error loading config file.') from err
+
+
+def pytest_configure(config: Config) -> None:
+    config_file_path = config.getoption('config_file_path', default=None)
+
+    if not config_file_path:
+        raise UsageError('No config file is specified, use --config option for that')
+
+    config.stash[config_file_key] = ConfigFilePlugin(config_file_path)
+    config.pluginmanager.register(config.stash[config_file_key])
+
+
+def pytest_unconfigure(config: Config) -> None:
+    config_file_plugin = config.stash.get(config_file_key, None)
+
+    if config_file_plugin:
+        del config.stash[config_file_key]
+        config.pluginmanager.unregister(config_file_plugin)
+
+
+@fixture(scope='session')
+def config(pytestconfig: Config):
+    """
+    Returns configuration object loaded from the configuration file
+    """
+    config_file_plugin = pytestconfig.stash.get(config_file_key, None)
+
+    config = {}
+
+    if config_file_plugin:
+        config = config | config_file_plugin.config
+
+    return config
+
+
+@fixture(scope='session')
+def host(config):
+    return config['host']
--- test.py
+++ test.py
@@ -1,54 +1,61 @@
 import requests
-import yaml
-from pytest import mark
+from pytest import mark, fixture, FixtureRequest
 from http import HTTPStatus
 
-
-HOST = 'https://service.verivox.de/'
 GET_CITIES = 'geo/latestv2/cities/{POSTCODE}'
 GET_STREETS = '/geo/latestv2/cities/{POSTCODE}/{CITY}/streets'
 
-with open('config.yaml') as config_file:
-    config = yaml.safe_load(config_file)
+
+@fixture
+def streets(request: FixtureRequest, config):
+    postcode = request.getfixturevalue('postcode')
+    city = request.getfixturevalue('city')
+
+    return next((c['streets']
+                 for c in next((p['cities']
+                                for p in config['postcodes']
+                                if str(p['value']) == str(postcode)))
+                 if c['name'] == city))
 
 
 @mark.parametrize('postcode, cities', [
-    (postcode['value'], [city['name']
-                         for city in postcode['cities']])
-    for postcode in config['postcodes']
+    ('10409', ['Berlin']),
+    ('77716', ['Fischerbach', 'Haslach', 'Hofstetten']),
 ])
-def test_get_cities(postcode, cities):
+def test_get_cities(postcode, cities, host):
     """
     GIVEN the address checking service endpoint: https://service.verivox.de/geo/latestv2/cities/POSTCODE
     WHEN I request the cities for postcode {10409}
          https://service.verivox.de/geo/latestv2/cities/10409/
     THEN I should receive a response with only one city: Berlin
     """
-    resp = requests.get(HOST + GET_CITIES.format(POSTCODE=postcode))
+    resp = requests.get(host + GET_CITIES.format(POSTCODE=postcode))
     assert resp.status_code == HTTPStatus.OK
     assert resp.json() == {'Cities': cities}
 
 
 @mark.parametrize('postcode', [
-    22333
+    '22333'
 ])
-def test_get_cities_not_found(postcode):
-    resp = requests.get(HOST + GET_CITIES.format(POSTCODE=postcode))
+def test_get_cities_not_found(postcode, host):
+    resp = requests.get(host + GET_CITIES.format(POSTCODE=postcode))
     assert resp.status_code == HTTPStatus.NOT_FOUND
     assert resp.content == b''
 
 
-@mark.parametrize('postcode, city, streets', [
-    (postcode['value'], city['name'], city['streets'])
-    for postcode in config['postcodes'] for city in postcode['cities']
+@mark.parametrize('postcode, city', [
+    ('10409', 'Berlin'),
+    ('77716', 'Fischerbach'),
+    ('77716', 'Haslach'),
+    ('77716', 'Hofstetten'),
 ])
-def test_get_streets(postcode, city, streets):
+def test_get_streets(postcode, city, streets, host):
     """
     GIVEN the address checking service endpoint: https://service.verivox.de/geo/latestv2/cities/
     WHEN I request the streets for {Berlin} postcode {10409}
             https://service.verivox.de/geo/latestv2/cities/10409/Berlin/streets
     THEN I should receive a response with 29 streets
     """
-    resp = requests.get(HOST + GET_STREETS.format(POSTCODE=postcode, CITY=city))
+    resp = requests.get(host + GET_STREETS.format(POSTCODE=postcode, CITY=city))
     assert resp.status_code == HTTPStatus.OK
     assert resp.json() == {'Streets': streets}
